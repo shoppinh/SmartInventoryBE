5cf01b5156cdd5d62ca6a819ce3f38c0eb47c98c
kien.mac <mactrungkien2000@gmail.com>
2024-11-14 00:36:44 +0700

    Add duplicate comment for PUT method in ShippingsController

--- Controllers/ShippingsController.cs
+++ Controllers/ShippingsController.cs
@@ -43,6 +43,7 @@ namespace SmartInventoryBE.Controllers
 
         // PUT: api/Shippings/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // PUT: api/Shippings/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutShipping(int id, Shipping shipping)
         {
